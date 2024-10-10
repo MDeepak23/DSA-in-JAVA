2d9c03e946a4151b6ded09d9e227ac9dd3bcc142
DEEPAK MANDAPALLI <152033387+MDeepak23@users.noreply.github.com>
2024-10-10 20:12:44 +0530

    Update Maxsub.java

--- arrays/Maxsub.java
+++ arrays/Maxsub.java
@@ -11,13 +11,13 @@ public class Maxsub{
         }
           return Max;
     }
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        for(int i=0;i<n;i++)
-        a[i] = sc.nextInt();
-        Maxsub obj = new Maxsub();
-        System.out.println(obj.maxsub(a,n));
+public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     int n = sc.nextInt();
+     int a[] = new int[n];
+     for(int i=0;i<n;i++)
+     a[i] = sc.nextInt();
+     Maxsub obj = new Maxsub();
+     System.out.println(obj.maxsub(a,n));
      }
 }
