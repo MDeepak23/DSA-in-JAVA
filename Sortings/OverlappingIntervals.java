dc8d5b9dd64ba7a90c82a26bfabd73235706be9e
DEEPAK MANDAPALLI <152033387+MDeepak23@users.noreply.github.com>
2025-10-06 22:11:41 +0530

    Create OverlappingIntervals.java

---
+++ Sortings/OverlappingIntervals.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class OverlappingIntervals {
+
+    static ArrayList<int[]> mergeOverlap(int[][] arr) {
+
+        Arrays.sort(arr, (a, b) -> Integer.compare(a[0], b[0]));
+
+        ArrayList<int[]> res = new ArrayList<>();
+        res.add(new int[]{arr[0][0], arr[0][1]});
+
+        for (int i = 1; i < arr.length; i++) {
+            int[] last = res.get(res.size() - 1);
+            int[] curr = arr[i];
+
+           
+            if (curr[0] <= last[1])
+                last[1] = Math.max(last[1], curr[1]);
+            else
+                res.add(new int[]{curr[0], curr[1]});
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[][] arr = {{7, 8}, {1, 5}, {2, 4}, {4, 6}};
+        ArrayList<int[]> res = mergeOverlap(arr);
+
+        for (int[] interval : res)
+            System.out.println(interval[0] + " " + interval[1]);
+    }
+}
