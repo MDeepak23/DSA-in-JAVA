0ecf75538a3692f4a27777d6fa3240e34c1bd277
DEEPAK MANDAPALLI <152033387+MDeepak23@users.noreply.github.com>
2025-10-25 23:14:04 +0530

    Update CountDaysWithoutMeeting.java

--- leetcode-potd/CountDaysWithoutMeeting.java
+++ leetcode-potd/CountDaysWithoutMeeting.java
@@ -1,3 +1,27 @@
+//3169. Count Days Without Meetings
+
+/*
+You are given a positive integer days representing the total number of days an employee is available for work (starting from day 1). You are also given a 2D array meetings of size n where, meetings[i] = [start_i, end_i] represents the starting and ending days of meeting i (inclusive).
+
+Return the count of days when the employee is available for work but no meetings are scheduled.
+
+Note: The meetings may overlap.
+
+ 
+
+Example 1:
+
+Input: days = 10, meetings = [[5,7],[1,3],[9,10]]
+
+Output: 2
+
+Explanation:
+
+There is no meeting scheduled on the 4th and 8th days.
+
+*/
+
+
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -32,4 +56,5 @@ public class CountDaysWithoutMeeting {
         System.out.println(countDays(days, meetings));
     }
 }    
+
 
