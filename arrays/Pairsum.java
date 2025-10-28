011975a6d3cc8fcda025ef4da0e3900c2ac8b3aa
DEEPAK MANDAPALLI <152033387+MDeepak23@users.noreply.github.com>
2025-10-28 12:05:11 +0530

    Refactor Pairsum to implement twoSum method

--- arrays/Pairsum.java
+++ arrays/Pairsum.java
@@ -1,31 +1,24 @@
 // leetcode twosum problem 
 import java.util.*;
-public class Pairsum{
-    public void pairsum(int a[],int n,int target){
-        int i=0,j=n-1;
-        while(i<j){
-            int p = a[i]+a[j];
-            if(p >target)
-                j--;
-            else if(p < target)
-                i++;
-            else {
-                System.out.println(a[i]+","+a[j]);
-                break;
-            }
-                
 
+class Pairsum{
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> a = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int comple = target - nums[i];
+            if (a.containsKey(comple)) {
+                return new int[]{a.get(comple), i};
+            }
+            a.put(nums[i], i);
         }
+        return new int[]{};
     }
- public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int a[] = new int[n];
-        for(int i=0;i<n;i++)
-            a[i] = sc.nextInt();
-        Arrays.sort(a);
-        int target = sc.nextInt();
-        Pairsum obj = new Pairsum();
-        obj.pairsum(a,n,target);
-     }
+
+    // Example test
+    public static void main(String[] args) {
+        
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        System.out.println(Arrays.toString(twoSum(nums, target))); // Output: [0, 1]
+    }
 }
